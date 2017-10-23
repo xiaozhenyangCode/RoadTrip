dc202fb2e9f81b150ebcd5872a76ae3b3fd0ddba
xiaozhenyangcode <xiaozheyangcode@163.com>
2017-10-23 18:25:10 +0800

    实时导航测试

--- RoadTrip/ViewController.m
+++ RoadTrip/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 #import <AMapNaviKit/AMapNaviKit.h>
-@interface ViewController ()<AMapNaviDriveManagerDelegate>
+@interface ViewController ()<AMapNaviDriveManagerDelegate,AMapNaviDriveViewDelegate>
 @property(nonatomic,strong)AMapNaviDriveManager *driveManager;
 @property(nonatomic,strong)AMapNaviDriveView *driveView;
 
@@ -18,9 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initDriveView];
+
     [self initDriveManager];
     [self configDriveNavi];
 
+}
+- (void)initDriveView
+{
+    if (self.driveView == nil)
+    {
+        self.driveView = [[AMapNaviDriveView alloc] initWithFrame:self.view.bounds];
+        self.driveView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        [self.driveView setDelegate:self];
+        
+        [self.view addSubview:self.driveView];
+    }
 }
 - (void)initDriveManager
 {
@@ -32,8 +45,16 @@
 }
 - (void)configDriveNavi
 {
+    //将driveView添加为导航数据的Representative，使其可以接收到导航诱导数据
     [self.driveManager addDataRepresentative:self.driveView];
     [self.view addSubview:self.driveView];
+}
+- (void)driveManagerOnCalculateRouteSuccess:(AMapNaviDriveManager *)driveManager
+{
+    NSLog(@"onCalculateRouteSuccess");
+    
+    //算路成功后开始GPS导航
+    [self.driveManager startGPSNavi];
 }
 //- (void)calculateRoute
 //{
